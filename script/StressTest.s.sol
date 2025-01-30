786270f534599f4c161140945ca27ad1abea736f
Calnix <calnix.289@gmail.com>
2025-01-30 14:28:02 +0700

    gas testing

--- script/StressTest.s.sol
+++ script/StressTest.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {StakingPro} from "../src/StakingPro.sol";
+import {StakingPro, DataTypes} from "../src/StakingPro.sol";
 
 import {RewardsVaultV1} from "../src/RewardsVaultV1.sol";
 
@@ -12,6 +12,9 @@ import {ERC20Mock} from "./../lib/openzeppelin-contracts/contracts/mocks/token/E
 
 // interface
 import {IERC20} from "./../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+// cheats
+import "forge-std/Test.sol";
 
 // base sepolia
 abstract contract Contracts is Script {
@@ -51,7 +54,7 @@ contract DeployContracts is Contracts {
         // constructor params
         address stakedToken = address(mockMoca);
         address registry = address(mockRegistry);       
-        uint256 startTime = block.timestamp + 5 minutes;
+        uint256 startTime = block.timestamp + 1 minutes;
         uint256 nftMultiplier = 1000; // 10% boost
         uint256 creationNftsRequired = 5;
         uint256 vaultCoolDownDuration = 7 days;
@@ -66,8 +69,10 @@ contract DeployContracts is Contracts {
 
         rewardsVault = new RewardsVaultV1(deployer, owner, owner, address(pool));
 
-        // connect pool and rewardsVault
+        // connect pool to rewardsVault
         pool.setRewardsVault(address(rewardsVault));
+        // connect registry to pool
+        mockRegistry.setPool(address(pool));
 
         // setup distribution 0: staking power
         pool.setupDistribution(
@@ -111,22 +116,22 @@ contract DeployContracts is Contracts {
 
 // forge script script/StressTest.s.sol:DeployContracts --rpc-url base_sepolia --broadcast --verify -vvvvv --etherscan-api-key base_sepolia
 
-
+// note update addresses
 abstract contract DeployedContracts is Script {
 
     // staking assets
-    ERC20Mock public mockMoca = ERC20Mock(0x9fa21d97DC3251b27D7a27250AFE3D17E7c54400);
-    MockRegistry public mockRegistry = MockRegistry(0x902981DD2999638Ba02557B64E3592834D674458);
+    ERC20Mock public mockMoca = ERC20Mock(0x8528c2DF4eD0Cf397a31B3F0ff0B96719A1bE2C6);
+    MockRegistry public mockRegistry = MockRegistry(0xA13A3eEfF3b143635f88110360d14e9b1068ce0D);
     
     // staking assets
-    StakingPro public pool = StakingPro(0x2b3dC91EBfa167470B25f49Ec6DB8b7052cA423D); 
-    RewardsVaultV1 public rewardsVault = RewardsVaultV1(0x685D117D88cE4B1104dc35f935Dd233733E182DB);
+    StakingPro public pool = StakingPro(0x14f2a22C97AE2890E73CB15B1C0E42A8a0821223); 
+    RewardsVaultV1 public rewardsVault = RewardsVaultV1(0x264541e2Dc34875943a746174e862aC594164AA2);
 
     // rewards assets
-    ERC20Mock public rewardToken = ERC20Mock(0x60Ea1BEBD8eac57C4c0ed0A16FC6735cBB72ae2C);
+    ERC20Mock public rewardToken = ERC20Mock(0x58bDE6071DB4a9B006FdAbe75a2eeB43DEcb779B);
 
     // VAULT ID
-    bytes32 public vaultId;
+    bytes32 public vaultId = bytes32(0x5611645c7b7387fb8499426a4c76a74c7241f751aca035dcabb5e9b0ed7cf68b);
 
     address public deployer = 0x8C9C001F821c04513616fd7962B2D8c62f925fD2;
 
@@ -141,18 +146,20 @@ abstract contract DeployedContracts is Script {
 contract CreateVault is DeployedContracts {
 
     function run() public broadcast_TestKey {
+        // create vault
         uint256[] memory tokenIds = new uint256[](5);
             tokenIds[0] = 0;
             tokenIds[1] = 1;
             tokenIds[2] = 2;
             tokenIds[3] = 3;
             tokenIds[4] = 4;
-        pool.createVault(tokenIds, 1000, 1000, 1000);
+        pool.createVault(tokenIds, 1000, 1000, 1000);  
     }
 }
 
 // forge script script/StressTest.s.sol:CreateVault --rpc-url base_sepolia --broadcast --verify -vvvvv --etherscan-api-key base_sepolia
-    /**
+
+    /** note:
         Increment number of distributions setup.
         For each increment, gas profile select user functions till exhaustion.
      */
@@ -167,13 +174,13 @@ contract SetupDistributions is DeployedContracts {
         return address(uint160(uint256(bytes32_)));
     }
 
-    function setupDistributionAndDeposit() public returns (uint256) {
+    function setupDistributionAndDeposit(uint256 delayInSeconds) public returns (uint256) {
         // get next distribution id
         uint256 distributionId = pool.getActiveDistributionsLength();
 
         // generic distribution params
         uint256 duration = 90 days;
-        uint256 startTime = block.timestamp;
+        uint256 startTime = block.timestamp + delayInSeconds;   //note: block.timestamp ends up being a delayed value
         uint256 endTime = startTime + duration;
 
         uint256 totalRewards = 90 ether;
@@ -199,16 +206,39 @@ contract SetupDistributions is DeployedContracts {
         pool.stakeTokens(vaultId, 90 ether);
     }
 
-    function run() public broadcast_TestKey {
+    function loop() public {
 
-//        while (true) {
-            uint256 distributionId = setupDistributionAndDeposit();    
+        while (true) {
+            uint256 distributionId = setupDistributionAndDeposit(90);    
             console.log("Distribution setup ID:", distributionId);
+            
+            vm.sleep(30_000);
 
-            stakeTokens();
-            console.log("Tokens staked");
-//        }
+            (,,,uint256 distributionStartTimestamp,,,,,) = pool.distributions(distributionId);
+
+            if (distributionStartTimestamp < block.timestamp) {
+                console.log("Distribution not started yet", distributionId);
+
+                // Sleeps for a given amount of milliseconds
+                uint256 timeToSleepSeconds = distributionStartTimestamp - block.timestamp;
+                vm.sleep((timeToSleepSeconds + 1) * 1000);
+
+            } else{
+
+                console.log("Distribution started", distributionId);
+                stakeTokens();
+                console.log("Tokens staked");
+            }
+        }
+    }
+
+    function run() public broadcast_TestKey {
+        stakeTokens();
+        console.log("Tokens staked");
+
+        uint256 distributionId = setupDistributionAndDeposit(180);    //note: 150 seconds delay with 8 distributions. started at 60
+        console.log("Distribution setup ID:", distributionId);
     }
 }
 
-// forge script script/StressTest.s.sol:StressTest --rpc-url base_sepolia --broadcast --verify -vvvvv --etherscan-api-key base_sepolia
+// forge script script/StressTest.s.sol:SetupDistributions --rpc-url base_sepolia --broadcast --verify -vvvvv --etherscan-api-key base_sepolia
